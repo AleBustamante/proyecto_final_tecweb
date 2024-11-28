69986e189ce2f935c18acde76ba754838e04f842
Alejandro Bustamante <adbdc.u@gmail.com>
2024-11-28 10:34:40 -0400

    Working basic connection to the db and db filled with data

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	//"net/http"
+
+	//"github.com/gin-gonic/gin"
+
+	"github.com/AleBustamante/proyecto_final_tecweb/tree/main/backend/db"
+
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
+)
+
+// Response es la estructura que devolveremos como JSON
+type Response struct {
+	Message string `json:"message"`
+}
+
+type Product struct {
+	ID          int
+	Name        string
+	Description string
+	Price       float32
+	Stock       int
+	Created_at  int
+	Updated_at  int
+}
 
 func main() {
-	fmt.Println("vim-go")
+	db.ConnectToDb()
+	// Creamos un router de Gin
+	//router := gin.Default()
+
+	// Definimos la ruta GET /hola
+	//router.GET("/hola", func(c *gin.Context) {
+	//response := Response{
+	//Message: "¡Hola desde el servidor Go con Gin!",
+	//}
+
+	//c.JSON(http.StatusOK, response)
+	//})
+	//router.POST("/insert", func(c *gin.Context) {
+	//response := Response{
+	//Message: "This is a confirmation message",
+	//}
+
+	//c.JSON(http.StatusOK, response)
+	//})
+
+	// Iniciamos el servidor en el puerto 8080
+	//router.Run(":8080")
 }
