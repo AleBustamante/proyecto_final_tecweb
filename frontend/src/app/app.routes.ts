1235efafc481fe448a0cf8e96f24800492c009a3
Alejandro Bustamante <adbdc.u@gmail.com>
2024-12-04 12:45:35 -0400

    Added login and register components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,22 @@ import { MovieListComponent } from './paginas/movie-list/movie-list.component';
 import { AddWatchlistComponent } from './paginas/add-watchlist/add-watchlist.component';
 
 export const routes: Routes = [
-    {path: 'buscar-peliculas', component: MovieListComponent},
-    {path: 'agregar-peliculas', component: AddWatchlistComponent}
+    {
+      path: 'buscar-peliculas',
+      component: MovieListComponent
+    },
+    {
+      path: 'agregar-peliculas',
+      component: AddWatchlistComponent
+    },
+      {
+    path: 'register',
+    loadComponent: () => import('./paginas/register/register.component')
+      .then(m => m.RegisterComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./paginas/login/login.component')
+      .then(m => m.LoginComponent)
+  },
 ];
